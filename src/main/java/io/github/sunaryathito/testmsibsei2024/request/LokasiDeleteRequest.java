868c337e7e6a040ca66a3d09fc8a0a86c492f2ed
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-08-16 16:05:29 +0700

    Added lokasi delete request

---
+++ src/main/java/io/github/sunaryathito/testmsibsei2024/request/LokasiDeleteRequest.java
@@ -0,0 +1,12 @@
+package io.github.sunaryathito.testmsibsei2024.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LokasiDeleteRequest {
+    private int id;
+}
