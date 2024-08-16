14ccbdbc59baad830ebbbd4061198324110ab092
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-08-16 16:04:57 +0700

    Added lokasi repository

---
+++ src/main/java/io/github/sunaryathito/testmsibsei2024/repository/LokasiRepository.java
@@ -0,0 +1,7 @@
+package io.github.sunaryathito.testmsibsei2024.repository;
+
+import io.github.sunaryathito.testmsibsei2024.entity.LokasiEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LokasiRepository extends CrudRepository<LokasiEntity, Integer> {
+}
