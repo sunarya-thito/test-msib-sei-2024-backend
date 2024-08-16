4a02679ff3209030b3334a13b94ea4cb23daf52d
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-08-16 16:05:06 +0700

    Added proyek repository

---
+++ src/main/java/io/github/sunaryathito/testmsibsei2024/repository/ProyekRepository.java
@@ -0,0 +1,7 @@
+package io.github.sunaryathito.testmsibsei2024.repository;
+
+import io.github.sunaryathito.testmsibsei2024.entity.ProyekEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProyekRepository extends CrudRepository<ProyekEntity, Integer> {
+}
