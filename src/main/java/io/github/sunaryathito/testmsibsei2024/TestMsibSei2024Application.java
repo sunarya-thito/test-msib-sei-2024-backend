e11e161a5f42118bef507ea5c7500e861c706408
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-08-16 16:03:52 +0700

    Initial commit

---
+++ src/main/java/io/github/sunaryathito/testmsibsei2024/TestMsibSei2024Application.java
@@ -0,0 +1,13 @@
+package io.github.sunaryathito.testmsibsei2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMsibSei2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMsibSei2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/sunaryathito/testmsibsei2024/TestMsibSei2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sunaryathito.testmsibsei2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMsibSei2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
